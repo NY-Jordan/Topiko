1080ca66f7e33c25ff8f99df8f345f7402955946
Nghokeng Daniel <nghokend@gmail.com>
2023-06-24 14:08:38 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/topiko_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.topiko_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:topiko_customer/app.dart';
+
+void main() {
+  runApp(const App());
+}
